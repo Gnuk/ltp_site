9a489f9c43319241ec387c63fdff3a053aff2ac1
Gnuk <gnuk@mailoo.org>
2012-10-07 17:03:01 +0200

    Test plus poussés d'OpenLayer

--- config/links.php
+++ config/links.php
@@ -5,4 +5,6 @@
 	define('LINK_STYLES', LINK_ROOT . LINKR_STYLES);
 	define('LINK_ERROR404', LINK_ROOT . LINK_R . 'error404.html');
 	define('LINK_PAGES', LINK_ROOT . LINK_R . 'pages/');
+	define('LINKR_IMAGES', LINK_R . 'images/');
+	define('LINK_IMAGES', LINKR_IMAGES . 'images/');
 ?>
--- pages/osm.page.php
+++ pages/osm.page.php
@@ -1,40 +1,67 @@
 <?php
 	if(Page::haveRights()){
 ?>
-<html><body>
-  <div id="mapdiv"></div>
-  <script src="lib/openlayers/OpenLayers.js"></script>
-  <script>
-    map = new OpenLayers.Map("mapdiv");
-    map.addLayer(new OpenLayers.Layer.OSM());
- 
-    var position1 = new OpenLayers.LonLat( 5.92 ,45.57 )
-          .transform(
-            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-            map.getProjectionObject() // to Spherical Mercator Projection
-          );
-    var position2 = new OpenLayers.LonLat( 5.8714950, 45.6470858 )
-          .transform(
-            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-            map.getProjectionObject() // to Spherical Mercator Projection
-          );
-    var center = new OpenLayers.LonLat( 5.8714950, 45.6 )
-          .transform(
-            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-            map.getProjectionObject() // to Spherical Mercator Projection
-          );
- 
-    var zoom=12;
- 
-    var markers = new OpenLayers.Layer.Markers( "Markers" );
-    map.addLayer(markers);
- 
-    markers.addMarker(new OpenLayers.Marker(position1));
-    markers.addMarker(new OpenLayers.Marker(position2));
- 
-    map.setCenter (center, zoom);
-  </script>
-</body></html>
+<html>
+	<head>
+		<style>
+			#mapdiv{
+				height: 600px;
+				width: 800px;
+			}
+			.olPopup{
+				background: black;
+				width: 200px;
+				height: auto;
+				box-shadow: 0px 3px 5px black;
+			}
+			.olPopup, .olPopup .olpopupcontent{
+				border-radius: 10px;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="mapdiv"></div>
+		<script src="lib/openlayers/OpenLayers.js"></script>
+		<script>
+			var fromProjection = new OpenLayers.Projection("EPSG:4326");
+			var toProjection = new OpenLayers.Projection("EPSG:900913");
+			var options = {
+				controls: [
+				new OpenLayers.Control.Navigation(),
+				new OpenLayers.Control.PanZoomBar(),
+				new OpenLayers.Control.LayerSwitcher(),
+				new OpenLayers.Control.Attribution()
+				]
+			};
+			function addPosition(longitude, lattitude){
+				return new OpenLayers.LonLat( longitude ,lattitude ).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
+			}
+			function getIcon(){
+				var size = new OpenLayers.Size(21, 25);
+				var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+				return new OpenLayers.Icon('<?php echo LINKR_IMAGES . 'marker.png';?>',size,offset);
+			}
+			
+			map = new OpenLayers.Map("mapdiv", options);
+			map.addLayer(new OpenLayers.Layer.OSM());
+			
+		
+			var position1 = addPosition( 5.92 ,45.57 );
+			var position2 = addPosition( 5.8714950, 45.6470858 );
+			var center = addPosition( 5.8714950, 45.6 );
+		
+			var zoom=12;
+		
+			var markers = new OpenLayers.Layer.Markers( "<?php echo T_("Amis"); ?>" );
+			map.addLayer(markers);
+		
+			markers.addMarker(new OpenLayers.Marker(position1, getIcon()));
+			markers.addMarker(new OpenLayers.Marker(position2, getIcon()));
+		
+			map.setCenter (center, zoom);
+		</script>
+	</body>
+</html>
 <?php
 	}
 	else{

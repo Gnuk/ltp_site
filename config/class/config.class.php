e01755b73fb817930f7794bd5d7772a37e8fc71d
Gnuk <gnuk@mailoo.org>
2012-10-10 10:26:24 +0200

    Implémentation partielle des Sessions

--- config/class/config.class.php
+++ config/class/config.class.php
@@ -19,6 +19,7 @@ class Config{
 		self::$defaultLocale='en_US';
 		self::$localedomain='messages';
 		self::$locale=self::$defaultLocale;
+		self::setSessions();
 		self::setLocales();
 	}
 	
@@ -70,6 +71,75 @@ class Config{
 		T_textdomain(self::$localedomain);
 
 		header('Content-type: text/html; charset=' . self::$encoding);
+	}
+	
+	/**
+	* Récupère la session et l'initialise
+	*/
+	private static function setSessions(){
+		if(isset($_GET['deconnection'])){
+			self::deconnection();
+		}
+		session_start();
+		if(isset($_POST['login']) AND isset($_POST['password'])){
+			self::connect();
+		}
+		else if(empty($_SESSION)){
+			self::putSessions();
+		}
+	}
+	
+	/**
+	* Définit les sessions
+	* @param string $user Nom de l'utilisateur
+	* @param int $rights Droits de l'utilisateur
+	* @param int $id Identifiant utilisateur
+	*/
+	private static function putSessions($user='anonymous', $rights = 0, $id=null){
+		$_SESSION['user'] = $user;
+		$_SESSION['rights'] = $rights;
+		if(isset($id)){
+			$_SESSION['user_id'] = $id;
+		}
+	}
+	
+	/**
+	* Récupère les droits de l'utilisateur courant
+	* @return int
+	*/
+	public static function getRights(){
+		$rights = 0;
+		if(isset($_SESSION['rights'])){
+			$rights = $_SESSION['rights'];
+		}
+		return $rights;
+	}
+	
+	/**
+	* Indique si un utilisateur est connecté ou non
+	* @return boolean
+	*/
+	public static function isUser(){
+		if(isset($_SESSION['user_id'])){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	* Vérification de l'utilisateur
+	*/
+	public static function connect(){
+		self::putSessions();
+	}
+	
+	/**
+	* Déconnexion
+	*/
+	public static function deconnection(){
+		session_start();
+		$_SESSION = array();
+		session_destroy();
 	}
 }
 ?>

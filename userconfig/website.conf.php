3a45eb7df3c25d1773a22e03c4d24f13b14f694d
Gnuk <gnuk@mailoo.org>
2012-10-28 10:14:26 +0100

    Ajout du fichier website.conf.php (non implémené pour le moment)

---
+++ userconfig/website.conf.php
@@ -0,0 +1,11 @@
+<?php
+	$params = array(
+		'title' => 'LocalizeTeaPots',
+		'description' => T_('Retrouve tous tes amis (pour le thé)'),
+		'keywords' => array(T_('localiser'), T_('amis'), T_('carte'), T_('position'), 'OpenStreetMap', 'map'),
+		'author' => 'Open Team Map',
+		'application-name' => 'LocalizeTeaPots Website',
+		'generator' => 'GNK Website',
+		'year' => '2012'
+	);
+?>

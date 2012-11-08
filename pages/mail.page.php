6ce4ef66063c6cfbe3f44f365ff06994900f9dfa
Gnuk <gnuk@mailoo.org>
2012-11-08 09:09:24 +0100

    PHPINFO pour voir les variables serveur

--- pages/mail.page.php
+++ pages/mail.page.php
@@ -1,5 +1,5 @@
 <?php
 	use \gnk\config\Tools;
 	echo T_('Test Send');
-	Tools::sendmail('gnuk@mailoo.org', 'Salut', 'Salut mon gars');
+ 	Tools::sendmail('gnuk@mailoo.org', 'Salut', 'Salut mon gars');
 ?>

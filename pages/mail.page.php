3a9397e3a1b66a090b88be0f90bac10f1225e045
Gnuk <gnuk@mailoo.org>
2012-11-08 12:34:08 +0100

    Affichage du résultat du mail

--- pages/mail.page.php
+++ pages/mail.page.php
@@ -1,5 +1,7 @@
 <?php
 	use \gnk\config\Tools;
 	echo T_('Test Send');
- 	Tools::sendmail('gnuk@mailoo.org', 'Salut', 'Salut mon gars');
+	echo '<pre>';
+ 	var_dump(Tools::sendmail('gnuk@mailoo.org', 'Salut', 'Salut mon gars'));
+ 	echo '</pre>';
 ?>
